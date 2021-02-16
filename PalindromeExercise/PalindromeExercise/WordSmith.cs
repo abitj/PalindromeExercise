d1173cdcf19b3a798893f66cf0d6df2e6bdbf0eb
abitj <https://github.com/abitj>
2021-02-16 11:20:07 -0600

    Completed unit testing for Plaindrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsPalindrome(string word)
+        {
+            var reverseWord = word.Reverse();
+            return word.ToLower().SequenceEqual(reverseWord);
+        }
+    }
+}
